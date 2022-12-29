504f25a341f36dc208cd3abdc2c604a629557326
ElvisStwar <elvis.stwar@hotmail.com>
2022-12-29 10:27:20 -0500

    Creacion de modulos y componentes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DadoComponent } from './elementos/dado/dado.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path: 'Dado', component: DadoComponent},
+  {path: '**', pathMatch: 'full', redirectTo: 'Dado'}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
